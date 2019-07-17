98f526a2ec21648271a14ffd01529235cba71be6
Archan Paul <archan.paul@arputer.com>
2019-07-17 13:15:13 +0530

    Fedora post-install script.

---
+++ fedora_custom_install.sh
@@ -0,0 +1,233 @@
+
+SOURCE=`pwd`/cache
+
+function fedora_upgrade() {
+	#sudo echo 'keepcache=1' | sudo tee -a /etc/dnf/dnf.conf
+	sudo dnf -y upgrade --downloadonly
+	sudo dnf -y upgrade
+}
+
+function httpd_service() {
+        sudo mkdir /home/public
+        sudo chmod 775 /home/public
+        sudo ln -s /home/public /var/www/html/public
+        sudo chcon -R --reference=/var/www/html /home/public
+        sudo systemctl enable httpd
+        sudo systemctl restart httpd
+}
+
+function misc_package() {
+	sudo dnf -y install mc
+	sudo dnf -y install vim emacs
+
+	sudo dnf -y install gnome-tweaks
+	sudo dnf -y install gnome-shell-extension-dash-to-dock
+
+
+	sudo dnf -y install autoconf automake make patch pkgconf libtool
+	sudo dnf -y install binutils bison flex gcc gcc-c++ gdb
+	sudo dnf -y install clang
+	sudo dnf -y install strace byacc elfutils ltrace strace oprofile valgrind 
+	sudo dnf -y install git
+	sudo dnf -y install golang
+	sudo dnf -y install glibc-devel libstdc++-devel kernel-devel
+	sudo dnf -y install protobuf protobuf-compiler
+
+	sudo dnf -y install docker kubernetes
+
+	##sudo dnf -y install mongodb
+	sudo dnf -y install couchdb
+	
+	sudo dnf -y install nmap traceroute
+
+	sudo dnf -y install gimp inkscape blender
+	sudo dnf -y install chromium thunderbird transmission
+	sudo dnf -y install gnome-books
+
+	sudo dnf -y install x264
+
+	sudo dnf -y install java-openjdk-devel
+}
+
+#RPMFusion
+function rpmfusion_source() {
+	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+}
+function rpmfusion_packages() {
+	sudo dnf -y install x264
+}
+
+function go_packages() {
+	sudo dnf -y install golang
+
+        sudo rm -rf /opt/go-packages
+        sudo mkdir /opt/go-packages
+
+        sudo chown -R root:wheel /opt/go-packages
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/go-packages
+
+        cat <<EOF | sudo tee /etc/profile.d/go-packages.sh
+export GOPATH=/opt/go-packages
+export PATH=\$PATH:\$GOPATH/bin
+EOF
+        source /etc/profile.d/go-packages.sh
+
+	## VSCode go plugin dependency
+	go get -u -v github.com/mdempsky/gocode
+	go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+	go get -u -v github.com/mdempsky/gocode
+	go get -u -v github.com/ramya-rao-a/go-outline
+	go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+	go get -u -v github.com/ramya-rao-a/go-outline
+	go get -u -v github.com/acroca/go-symbols
+	go get -u -v golang.org/x/tools/cmd/guru
+	go get -u -v golang.org/x/tools/cmd/gorename
+	go get -u -v github.com/go-delve/delve/cmd/dlv 
+	go get -u -v github.com/stamblerre/gocode
+	go get -u -v github.com/rogpeppe/godef
+	go get -u -v github.com/sqs/goreturns
+	go get -u -v golang.org/x/lint/golint
+	go get -u -v github.com/stamblerre/gocode
+	go get -u -v github.com/stamblerre/gocode
+
+	## Dev tools
+	go get -u -v github.com/cespare/reflex
+	go get -u -v golang.org/x/...
+	#go get -u -v golang.org/x/tools/cmd/godoc
+	#go get -u -v golang.org/x/tools/cmd/gorename
+	#go get -u -v golang.org/x/tools/cmd/goimports
+	#go get -u -v golang.org/x/tools/go/analysis/...
+	## goMobile
+	go get -u -v golang.org/x/mobile/cmd/gobind
+	go get -u -v golang.org/x/mobile/cmd/gomobile
+	## HTTP
+	go get -u -v github.com/gin-gonic/gin
+	go get -u -v github.com/gin-gonic/contrib/...
+	#go get -u -v github.com/dgrijalva/jwt-go
+	## goNum
+	go get -u -v -t gonum.org/v1/gonum/...
+	## DB
+	go get -u -v gopkg.in/mgo.v2
+	go get github.com/lib/pq
+	## protobuf
+	go get -u github.com/golang/protobuf/protoc-gen-go
+	## gRPC
+	go get -u google.golang.org/grpc
+	## Update
+	#go get -u -v all
+
+	# Go CDK
+	go get gocloud.dev
+
+        sudo chown -R root:wheel /opt/go-packages
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/go-packages
+}
+
+function vscode_package() {
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+	dnf check-update
+	sudo dnf -y install code
+	#code --list-extensions | xargs -L 1 echo code --install-extension
+}
+
+function android-studio_package() {
+	ANDROID_STUDIO_RELEASE=3.4.0.18
+	ANDROID_STUDIO_VERSION=183.5452501
+	sudo rm -rf /opt/android-studio
+	sudo  mkdir -p /opt/android-studio
+
+	wget -c https://dl.google.com/dl/android/studio/ide-zips/${ANDROID_STUDIO_RELEASE}/android-studio-ide-${ANDROID_STUDIO_VERSION}-linux.tar.gz -P ./cache
+
+	sudo tar zxfv ${SOURCE}/android-studio-ide-${ANDROID_STUDIO_VERSION}-linux.tar.gz -C /opt/android-studio
+	sudo chown -R root:wheel /opt/android-studio
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/android-studio
+
+	        cat <<EOF | sudo tee /opt/android-studio/android-studio.desktop
+[Desktop Entry]
+Type=Application
+Name=Android Studio
+Icon=/opt/android-studio/bin/studio.png
+Exec=env _JAVA_OPTIONS=-Djava.io.tmpdir=/var/tmp \
+/opt/android-studio/bin/studio.sh
+Terminal=false
+Categories=Development;IDE;
+EOF
+
+	sudo mkdir /opt/android-sdk
+	sudo chown -R root:wheel /opt/android-sdk
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/android-sdk
+
+	cat <<EOF | sudo tee /etc/profile.d/android-sdk.sh
+export ANDROID_HOME=/opt/android-sdk/
+export ANDROID_SDK_ROOT=\$ANDROID_HOME
+export ANDROID_NDK_ROOT=\$ANDROID_HOME/ndk-bundle
+export PATH=\$PATH:\$ANDROID_HOME/platform-tools/
+EOF
+
+        sudo ln -sf /opt/android-studio/android-studio.desktop /usr/share/applications/android-studio.desktop
+	source /etc/profile.d/android-sdk.sh
+}
+
+function dart-sdk_package() {
+	wget -c https://storage.googleapis.com/dart-archive/channels/stable/release/2.4.0/sdk/dartsdk-linux-x64-release.zip -P ./cache
+	sudo rm -rf /opt/dart-sdk
+
+	sudo unzip ${SOURCE}/dartsdk-linux-x64-release.zip -d /opt/
+	sudo mkdir /opt/dart-sdk/pub_cache
+	sudo chown -R root:wheel /opt/dart-sdk
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/dart-sdk
+
+	cat <<EOF | sudo tee /etc/profile.d/dart-sdk.sh
+#export PUB_CACHE=/opt/dart-sdk/pub_cache
+export PATH=\$PATH:/opt/dart-sdk/bin
+EOF
+	source /etc/profile.d/dart-sdk.sh
+}
+
+function flutter-sdk_package() {
+	#sudo dnf -y install libstdc++.i686
+
+	sudo rm -rf /opt/flutter-sdk
+	sudo mkdir -p /opt/flutter-sdk
+
+	#sudo git clone -b stable --single-branch https://github.com/flutter/flutter.git /opt/flutter-sdk --depth=1
+	sudo git clone -b master https://github.com/flutter/flutter.git /opt/flutter-sdk 
+	sudo mkdir /opt/flutter-sdk/pub_cache
+	sudo chown -R root:wheel /opt/flutter-sdk
+	sudo chmod -R u+rwX,go+rwX,o-w /opt/flutter-sdk
+
+	cat <<EOF | sudo tee /etc/profile.d/flutter-sdk.sh
+export FLUTTER_ROOT=/opt/flutter-sdk/
+export PUB_CACHE=\$FLUTTER_ROOT/pub_cache
+export ENABLE_FLUTTER_DESKTOP=true
+export PATH=\$PATH:\$FLUTTER_ROOT/bin:\$PUB_CACHE/bin
+EOF
+	source /etc/profile.d/flutter-sdk.sh
+}
+
+function gnome_themes() {
+	#sudo dnf -y copr enable daniruiz/flat-remix
+	#sudo dnf -y install flat-remix
+	sudo dnf -y install gtk-murrine-engine gtk2-engines
+}
+
+function docker() {
+	sudo groupadd docker 
+	sudo gpasswd -a ${USER} docker 
+	sudo systemctl restart docker
+}
+
+# fedora_upgrade
+# misc_package
+
+# rpmfusion_source
+## rpmfusion_packages
+
+# vscode_package
+# android-studio_package
+# dart-sdk_package
+# flutter-sdk_package
+# go_packages
+# gnome_themes
+# docker
