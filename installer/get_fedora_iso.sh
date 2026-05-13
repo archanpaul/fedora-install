63669325c67d56f1fdd2c47c51efca435aa9edbc
Archan Paul <archan.paul@gmail.com>
2026-05-13 16:08:44 +0530

    conf updated.

--- installer/get_fedora_iso.sh
+++ installer/get_fedora_iso.sh
@@ -1,5 +1,7 @@
 # Fedora Everything Netinstall
-ARIA2C_DOWNLOADER="aria2c -c -s 16 -x 16 -k 1M -j 1 "
-${ARIA2C_DOWNLOADER} https://download.fedoraproject.org/pub/fedora/linux/releases/44/Everything/x86_64/iso/Fedora-Everything-44-1.7-x86_64-CHECKSUM
-${ARIA2C_DOWNLOADER} https://download.fedoraproject.org/pub/fedora/linux/releases/44/Everything/x86_64/iso/Fedora-Everything-netinst-x86_64-44-1.7.iso
-
+# ARIA2C_DOWNLOADER="aria2c -c -s 16 -x 16 -k 1M -j 1 "
+WGET_DOWNLOADER="wget --show-progress -nc "
+${WGET_DOWNLOADER} https://download.fedoraproject.org/pub/fedora/linux/releases/44/Everything/x86_64/iso/Fedora-Everything-44-1.7-x86_64-CHECKSUM
+${WGET_DOWNLOADER} https://download.fedoraproject.org/pub/fedora/linux/releases/44/Everything/x86_64/iso/Fedora-Everything-netinst-x86_64-44-1.7.iso
+${WGET_DOWNLOADER} https://dl.fedoraproject.org/pub/fedora/linux/releases/44/Workstation/x86_64/iso/Fedora-Workstation-44-1.7-x86_64-CHECKSUM
+${WGET_DOWNLOADER} https://download.fedoraproject.org/pub/fedora/linux/releases/44/Workstation/x86_64/iso/Fedora-Workstation-Live-44-1.7.x86_64.iso
--- installer/verify_checksum.sh
+++ installer/verify_checksum.sh
@@ -1,3 +1,3 @@
 curl -O https://fedoraproject.org/fedora.gpg
-gpgv --keyring ./fedora.gpg Fedora-Everything-43-1.6-x86_64-CHECKSUM
-sha256sum --ignore-missing -c Fedora-Everything-43-1.6-x86_64-CHECKSUM
+gpgv --keyring ./fedora.gpg Fedora-Everything-44-1.7-x86_64-CHECKSUM
+sha256sum --ignore-missing -c Fedora-Everything-44-1.7-x86_64-CHECKSUM
