21de85f0a359403b161ffb0c59361f29cdda1010
Archan Paul <archan.paul@gmail.com>
2026-07-29 12:23:01 +0530

    download installer updated.

--- installer/verify_checksum.sh
+++ installer/verify_checksum.sh
@@ -1,3 +1,1 @@
-curl -O https://fedoraproject.org/fedora.gpg
-gpgv --keyring ./fedora.gpg Fedora-Everything-44-1.7-x86_64-CHECKSUM
-sha256sum --ignore-missing -c Fedora-Everything-44-1.7-x86_64-CHECKSUM
+gpgv --keyring ./fedora.gpg --output - Fedora-Everything-44-1.7-x86_64-CHECKSUM | sha256sum -c --ignore-missing
